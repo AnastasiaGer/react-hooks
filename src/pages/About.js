589e2cd0c4c17f755e56fcc9f5de4f09f3219cf4
AnastasiaGer <aagermanova@gmail.com>
2021-09-19 19:01:13 -0700

    Block 13 - L3

--- src/pages/About.js
+++ src/pages/About.js
@@ -2,8 +2,11 @@ import React from "react";
 
 export const About = () => {
   return (
-    <div>
-      <h1>About page</h1>
-    </div>
+    <figure class="figure">
+      <img src="https://images.unsplash.com/photo-1451187580459-43490279c0fa?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1400&q=80" class="figure-img img-fluid rounded" alt="..." width="500" height="300"/>
+      <figcaption class="figure-caption">
+        A caption for the above image.
+      </figcaption>
+    </figure>
   );
 };
